4e4c180020a3e9e25d6ab1cd789bb023d0cb79a1
kjx63 <ueyama.kenji.63@gmail.com>
2020-08-01 15:44:56 +0900

    ContactForm Component and Add Contact

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from 'react';
-import uuid from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 import ContactContext from './contactContext';
 import contactReducer from './contactReducer';
 import {
@@ -46,7 +46,10 @@ const ContactState = (props) => {
   const [state, dispatch] = useReducer(contactReducer, initialState);
 
   // Add Contact
-
+  const addContact = (contact) => {
+    contact.id = uuidv4();
+    dispatch({ type: ADD_CONTACT, payload: contact });
+  };
   // Delete Contact
 
   // Set Current Contact
@@ -66,6 +69,7 @@ const ContactState = (props) => {
       // including state and actions need to go in here
       value={{
         contacts: state.contacts,
+        addContact,
       }}
     >
       {props.children}
